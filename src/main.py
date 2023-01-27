30bc8e8669963829d78bb1e17ef819903f7c83f4
dragunwf <plarisanmarc@gmail.com>
2023-01-27 11:56:22 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,8 +4,10 @@ from scripts.utils import Utils
 
 
 def main():
+    print("Starting automation...")
     bot = Bot()
     bot.run()
+    print("Finished automation!")
 
 
 if __name__ == "__main__":
