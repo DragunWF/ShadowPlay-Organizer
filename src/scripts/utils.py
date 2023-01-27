b9411b43bb0db09eb39b168a6b78ec9cff233e11
dragunwf <plarisanmarc@gmail.com>
2023-01-27 11:35:52 +0800

    Update utils.py Add get_path() static method

--- src/scripts/utils.py
+++ src/scripts/utils.py
@@ -1,2 +1,12 @@
+import os
+
+
 class Utils:
-    pass
+    @staticmethod
+    def get_path() -> str:
+        dirname = __file__
+        root = os.path.abspath(os.path.join(os.path.dirname(dirname), ".."))
+        while "\\" in root:
+            root = root.replace("\\", "/")
+        root = "".join([i if i != ":" else f"{i}/" for i in root])
+        return f"{root[0].upper()}{root[1:]}"
