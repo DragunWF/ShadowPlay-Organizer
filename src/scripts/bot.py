4333c794be1917064c028ca19a599625f0a45c1d
dragunwf <plarisanmarc@gmail.com>
2023-01-27 12:04:54 +0800

    Update bot.py Checkpoint

--- src/scripts/bot.py
+++ src/scripts/bot.py
@@ -8,23 +8,28 @@ class Bot:
         self.image_extensions = ("png", "jpg", "jpeg")
         self.video_extensions = ("mp4", "mov")
 
-    def __filter_file(self, filename: str, look_for_images: bool) -> bool:
-        extension = filename.split(".")[-1]
+    def __filter_file(self, file_name: str, look_for_images: bool) -> bool:
+        extension = file_name.split(".")[-1]
         if look_for_images:
             return extension in self.image_extensions
         return extension in self.video_extensions
 
-    def __filter_image(self, filename: str) -> bool:
-        return self.__filter_file(filename, True)
+    def __filter_image(self, file_name: str) -> bool:
+        return self.__filter_file(file_name, True)
 
-    def __filter_video(self, filename: str) -> bool:
-        return self.__filter_file(filename, False)
+    def __filter_video(self, file_name: str) -> bool:
+        return self.__filter_file(file_name, False)
 
-    def run(self, path: str):  # recordings path
+    def __validate_directory(self, files: list, target_directory: str) -> None:
+        if target_directory in files:
+            os.mkdir(target_directory)
+
+    def run(self, path: str):
         self.path = path
 
         directories = os.listdir(self.path)
         for directory in directories:
             files = os.listdir(directory)
-            images = filter(self.__filter_image, os.listdir(directory))
-            videos = filter(self.__filter_video, os.listdir(directory))
+            images = filter(self.__filter_image, files)
+            videos = filter(self.__filter_video, files)
+            print(f"Organized {directory}!")
