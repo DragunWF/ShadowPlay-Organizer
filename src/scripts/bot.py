0892fef13c23b07ceb6c17509814e6f5f40b8b42
dragunwf <plarisanmarc@gmail.com>
2023-01-27 11:36:15 +0800

    Update bot.py Add base functionality

--- src/scripts/bot.py
+++ src/scripts/bot.py
@@ -1,2 +1,16 @@
+import os
+import shutil
+
+
 class Bot:
-    pass
+    def __init__(self):
+        self.path = None
+
+    def run(self, path):  # recordings path
+        self.path = path
+
+        directories = os.listdir(self.path)
+        for directory in directories:
+            files = os.listdir(directory)
+            for file in files:
+                pass
