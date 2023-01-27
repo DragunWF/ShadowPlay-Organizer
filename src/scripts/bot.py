b47144207522be757436fc0b16f78f4bb7eecd05
dragunwf <plarisanmarc@gmail.com>
2023-01-27 11:56:12 +0800

    Update bot.py Implement filtering

--- src/scripts/bot.py
+++ src/scripts/bot.py
@@ -5,12 +5,26 @@ import shutil
 class Bot:
     def __init__(self):
         self.path = None
+        self.image_extensions = ("png", "jpg", "jpeg")
+        self.video_extensions = ("mp4", "mov")
 
-    def run(self, path):  # recordings path
+    def __filter_file(self, filename: str, look_for_images: bool) -> bool:
+        extension = filename.split(".")[-1]
+        if look_for_images:
+            return extension in self.image_extensions
+        return extension in self.video_extensions
+
+    def __filter_image(self, filename: str) -> bool:
+        return self.__filter_file(filename, True)
+
+    def __filter_video(self, filename: str) -> bool:
+        return self.__filter_file(filename, False)
+
+    def run(self, path: str):  # recordings path
         self.path = path
 
         directories = os.listdir(self.path)
         for directory in directories:
             files = os.listdir(directory)
-            for file in files:
-                pass
+            images = filter(self.__filter_image, os.listdir(directory))
+            videos = filter(self.__filter_video, os.listdir(directory))
